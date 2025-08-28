4e802482639f74d6f3f141916270cf233559ee0f
Razvan Ciocan <rzv.c93@outlook.com>
2025-08-28 22:09:09 +0100

    Fix display issue in Chrome and update Vite config and homepage layout

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-// vite.config.ts
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+  base: '/',
   plugins: [react()],
   resolve: {
     alias: {
